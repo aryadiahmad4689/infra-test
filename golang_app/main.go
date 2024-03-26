13f408a5f431afa6108a5dece787e058d1dfb68c
padaelocomunity2 <sahrulalfa45@gmail.com>
2024-03-26 20:36:11 +0800

    update

--- golang_app/main.go
+++ golang_app/main.go
@@ -10,8 +10,7 @@ import (
 // handler yang akan dipanggil setiap kali ada request ke "/"
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	// Menulis respons "Hello, World!" ke browser
-	myVariable := viper.GetString("DATA_OKE") // Menggunakan nama variable tanpa prefix jika Anda tidak menetapkan SetEnvPrefix
-	fmt.Println(myVariable)
+
 	fmt.Fprintf(w, "Hello, World!"+myVariable)
 }
 
@@ -20,10 +19,9 @@ func main() {
 	viper.AutomaticEnv() // Membaca semua environment variables
 
 	http.HandleFunc("/", helloWorldHandler)
-	myVariable := viper.GetString("DATA_OKE") // Menggunakan nama variable tanpa prefix jika Anda tidak menetapkan SetEnvPrefix
-	fmt.Println(myVariable)
+
 	// Menjalankan server di port 8080
-	fmt.Println("Server is running on http://localhost:8099" + myVariable)
+	fmt.Println("Server is running on http://localhost:8099")
 	if err := http.ListenAndServe(":8099", nil); err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
