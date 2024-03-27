fe8d9b25b6d7e8b8fcf3cf2d33c409e33ce9ab20
padaelocomunity2 <sahrulalfa45@gmail.com>
2024-03-28 06:00:54 +0800

    update image

--- golang_app/main.go
+++ golang_app/main.go
@@ -24,6 +24,7 @@ func main() {
 
 	fmt.Println("Testing", viper.GetString("DATA_OKE"))
 	fmt.Println("Testing", os.Getenv("DATA_OKE"))
+	fmt.Println("Testing", os.Getenv("DATA_OKE"))
 
 	// Menjalankan server di port 8080
 	fmt.Println("Server is running on http://localhost:8099")
